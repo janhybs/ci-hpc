9a6b1a06d85f07f3a3a9250be78cb1bfe5f62121
Jan Hybš <jan.hybs@tul.cz>
2018-07-04 14:07:31 +0200

    Move to jupyter dir

--- ci-hpc/visualisation/jupyter/__init__.py
+++ ci-hpc/visualisation/jupyter/__init__.py
@@ -7,14 +7,15 @@ from utils.logging import logger
 import os
 import sys
 import matplotlib
+matplotlib.use('Agg')
+
 from utils.config import Config as cfg
 from datetime import datetime, timedelta
 from sklearn.preprocessing import normalize
 
-matplotlib.use('Agg')
 
 __dir__ = os.path.abspath(os.path.dirname(__file__))
-__root__ = os.path.dirname(os.path.dirname(__dir__))
+__root__ = os.path.dirname(os.path.dirname(os.path.dirname(__dir__)))
 __cfg__ = os.path.join(__root__, 'cfg')
 secret_path = os.path.join(__cfg__, 'secret.yaml')
 
@@ -29,7 +30,7 @@ import utils.data as datautils
 
 from matplotlib import pyplot as plt
 from artifacts.db import mongo
-from visualisation.utils.plot import tsplot, plot_mean
+from visualisation.utils.plot import tsplot, plot_mean, plot_mean_with_area
 
 cfg.init(secret_path)
 
@@ -88,13 +89,14 @@ def load_data(use_cache=True, filename='data.csv', generate=0, filters=dict()):
         },
         {
             "$project": {
-                "_id": 0,
+                "_id": 1,
                 "uuid": "$problem.uuid",
 
                 'git-branch': '$libs.branch',
                 'git-commit': '$libs.commit',
                 'git-timestamp': '$libs.timestamp',
                 'git-datetime': '$libs.datetime',
+                'returncode': '$problem.returncode',
 
                 "test-name": "$problem.test-name",
                 "case-name": "$problem.case-name",
@@ -118,11 +120,60 @@ def load_data(use_cache=True, filename='data.csv', generate=0, filters=dict()):
     return data
 
 
+def add_metrics(data):
+    data['commit-date'] = data['git-datetime'].apply(dateutils.long_format)
+    for g, d in data.groupby(['test-name', 'case-name']):
+        data.loc[d.index, 'duration-relative'] = normalise_vector(d['duration'], norm='max').T
+        # data.loc[d.index, 'duration-relative'] = d['duration']/d['duration'].max()
+
+        first_commit = sorted(list(set(d['git-datetime'])))[0]
+        max_first_commit = d[d['git-datetime'] == first_commit]['duration'].max()
+        data.loc[d.index, 'duration-relative-start'] = d['duration'] / max_first_commit
+    return data
+
+
+def dual_plot(conditions, *args, plot_func, error_kwargs, **kwargs):
+    ok_args = [a[conditions == 0] for a in args]
+    er_args = [a[conditions == 1] for a in args]
+    plot_func(*ok_args, **kwargs)
+    for k, v in error_kwargs.items():
+        kwargs[k] = v
+    plot_func(*er_args, **kwargs)
+
+
+def facetgrid_opts(data, x, y, z, x_space=15, aspect=2, sharex=False, sharey=False):
+    # g = sns.FacetGrid(subdata, row='case-name', sharey=False, sharex=sharex, size=3, aspect=2, hue='case-name')
+    # g = sns.FacetGrid(subdata, col='case-name', col_wrap=2, sharey=False, sharex=sharex, size=3, aspect=2, hue='case-name')
+
+    x_series, y_series = data[x], data[y]
+    col_wrap = 1 + int(np.floor(x_space / len(datautils.olist(x_series))))
+    inches = 8
+
+    if col_wrap == 1:
+        return dict(
+            row=y_series.name,
+            size=inches/aspect,
+            aspect=aspect,
+            hue=y_series.name,
+            sharey=sharey or False,
+            sharex=sharex or (True if z.find('relative') != -1 else False)
+        )
+    else:
+        return dict(
+            col=y_series.name,
+            col_wrap=col_wrap,
+            aspect=aspect,
+            size=int((inches-col_wrap+0.0)/aspect),
+            hue=y_series.name,
+            sharey=sharey or False,
+            sharex=sharex or (True if z.find('relative') != -1 else False)
+        )
+
 
 
 __all__ = [
     'np', 'pd', 'sns', 'plt', 'sc', 'w', 'logger', 'mongo', 'normalise_vector', 'normalise_matrix',
-    'tsplot', 'plot_mean',
+    'tsplot', 'plot_mean', 'plot_mean_with_area', 'dual_plot',
     'dateutils', 'datautils',
-    'load_data',
+    'load_data', 'add_metrics', 'facetgrid_opts',
 ]
