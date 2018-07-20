d8d9a1d09a851dbd5f9cc42aaffe90f879f122e1
Jan Hybš <jan.hybs@tul.cz>
2018-07-20 11:42:49 +0200

    Add more info when missing secrety.yaml file

--- ci-hpc/utils/config.py
+++ ci-hpc/utils/config.py
@@ -48,7 +48,8 @@ class Config(object):
         try:
             cls._cfg = yaml.load(open(config_file, 'r').read()) or dict()
         except Exception as e:
-            logger.warn_exception('Failed to load/parse %s configuration file, will use empty dict', config_file)
+            logger.warn('Failed to load/parse %s configuration file, will use empty dict', config_file)
+            logger.warn('You may want to create this file in order to use database connection', config_file)
             cls._cfg = dict()
         return cls._instance
 
