45d2558e838c6f8a2aa19db41956efddce80c3bb
Jan Hybš <jan.hybs@tul.cz>
2018-07-20 08:34:05 +0200

    Add more options when converting object to json

--- ci-hpc/visualisation/www/__init__.py
+++ ci-hpc/visualisation/www/__init__.py
@@ -7,22 +7,30 @@ from artifacts.db import mongo
 from bson import ObjectId
 import datetime
 import json
-import utils.dates as dateutils
+import utils.dateutils as dateutils
 import numpy as np
+import pandas as pd
 from flask_cors import CORS
 
 
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        if isinstance(o, np.ndarray):
+            return list(o)
+        if isinstance(o, pd.Series):
+            return list(o)
+        if isinstance(o, pd.DataFrame):
+            return o.values
+        if isinstance(o, datetime.datetime):
+            return dateutils.long_format(o)
+        if hasattr(o, 'to_json'):
+            return o.to_json()
+            # return o.isoformat()
+        return json.JSONEncoder.default(self, o)
+
 def init_flask_server():
-    class JSONEncoder(json.JSONEncoder):
-        def default(self, o):
-            if isinstance(o, ObjectId):
-                return str(o)
-            if isinstance(o, np.ndarray):
-                return list(o)
-            if isinstance(o, datetime.datetime):
-                return dateutils.long_format(o)
-                # return o.isoformat()
-            return json.JSONEncoder.default(self, o)
 
     app = Flask(__name__)
     app.config['RESTFUL_JSON'] = dict(
