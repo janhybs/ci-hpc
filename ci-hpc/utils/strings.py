7aee3ba99ed8ad86bc1019a102baf9cb45d708f5
Jan Hybš <x3mSpeedy@gmail.com>
2018-07-21 16:10:07 +0100

    Add pad_lines method

--- ci-hpc/utils/strings.py
+++ ci-hpc/utils/strings.py
@@ -4,5 +4,17 @@
 import random
 
 
-def generate_random_key(length):
+def generate_random_key(length=8):
+    """
+    :type length: int
+    """
     return ''.join(random.choice('0123456789abcdef') for _ in range(length))
+
+
+def pad_lines(string, pad=4):
+    """
+    :type pad: int or str
+    :type string: str
+    """
+    pad_str = pad if isinstance(pad, str) else int(pad) * ' '
+    return '\n'.join([pad_str + line for line in string.splitlines()])
