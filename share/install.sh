980e76d07ddefe19af75e0438c8a87285c676e58
Jan Hybš <jan.hybs@tul.cz>
2018-08-02 12:37:04 +0200

    Add pretty flag support

--- share/install.sh
+++ share/install.sh
@@ -3,4 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 # install 
-pip3 install -r $DIR/requirements.txt --upgrade ${1:---user}
+if [[ -z "$1" ]]; then
+  pip3 install -r $DIR/requirements.txt --upgrade --user
+else
+  pip3 install -r $DIR/requirements.txt --upgrade
+fi
