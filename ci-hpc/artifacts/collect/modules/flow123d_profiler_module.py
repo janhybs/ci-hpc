ce0bea0fc2c200fd4699d8397b364b528e393af8
Jan Hybs <jan.hybs@tul.cz>
2018-08-15 14:21:54 +0200

    Edit Flow123d module

--- ci-hpc/artifacts/collect/modules/flow123d_profiler_module.py
+++ ci-hpc/artifacts/collect/modules/flow123d_profiler_module.py
@@ -11,25 +11,6 @@ from artifacts.collect.modules import CollectResult, AbstractCollectModule, CIHP
 
 class CollectModule(AbstractCollectModule):
 
-    _floats = [
-        re.compile('cumul-time-.+'),
-        re.compile('percent'),
-        re.compile('timer-resolution'),
-    ]
-
-    _ints = [
-        re.compile('call-count-.+'),
-        re.compile('memory-.+'),
-        re.compile('file-line'),
-        re.compile('task-size'),
-        re.compile('run-process-count'),
-    ]
-
-    _dates = [
-        re.compile('run-started-at'),
-        re.compile('run-finished-at'),
-    ]
-
     _children = 'children'
 
     def process(self, object, from_file=None):
@@ -54,31 +35,40 @@ class CollectModule(AbstractCollectModule):
                     if p in status_json:
                         report.problem[p] = status_json[p]
 
-        # grab version while we're at it
-        report.git['version'] = object['program-version']
-        # grab task size as well
-        report.problem['task-size'] = int(object['task-size'])
+        if object:
+            # grab version while we're at it
+            report.git['version'] = object['program-version']
+            # grab task size as well
+            report.problem['task-size'] = int(object['task-size'])
 
-        # get cpu
-        cpus = float(object['run-process-count'])
-        report.problem['cpu'] = cpus
+            # get cpu
+            cpus = float(object['run-process-count'])
+            report.problem['cpus'] = cpus
 
-        # grab the root
-        root = object[self._children][0]
-        result = self._extract_important(root, cpus=cpus)
-        # and traverse the children
-        timers = self._traverse(root, list())
+            # grab the root
+            root = object[self._children][0]
+            result = self._extract_important(root, cpus=cpus)
+            # and traverse the children
+            timers = self._traverse(root, list())
 
-        # merge collected info with report
-        report.merge(dict(
-            result=result,
-            timers=timers,
-        ))
+            # merge collected info with report
+            report.merge(dict(
+                result=result,
+                timers=timers,
+            ))
 
         return CollectResult([report])
 
+    @classmethod
+    def _normalise_tag_name(cls, tag: str):
+        return re.sub('[_-]+', '-',
+                      re.sub("([a-z])([A-Z])", "\g<1>-\g<2>", tag)\
+                        .replace('::', '-')\
+                        .replace(' ', '-').lower()
+                      )
+
     def _traverse(self, root, result, path=''):
-        path = path + '/' + root['tag']
+        path = path + '/' + self._normalise_tag_name(root['tag'])
 
         item = self._extract_important(root)
         item['path'] = path
@@ -91,18 +81,21 @@ class CollectModule(AbstractCollectModule):
 
         return result
 
-    @staticmethod
-    def _extract_important(child, cpus=1.0):
+    @classmethod
+    def _extract_important(cls, child, cpus=1.0):
         sums = {
             'duration': 'cumul-time-sum',
-            'call-count': 'call-count-sum',
-            'alloc-count': 'memory-alloc-called-sum',
-            'dealloc-count': 'memory-dealloc-called-sum',
-            'alloc': 'memory-alloc-sum',
-            'dealloc': 'memory-dealloc-sum',
+            'executed': 'call-count-sum',
+            'cnt-alloc': 'memory-alloc-called-sum',
+            'cnt-dealloc': 'memory-dealloc-called-sum',
+            'mem-alloc': 'memory-alloc-sum',
+            'mem-dealloc': 'memory-dealloc-sum',
         }
         result = dict()
         result.update({k: float(child[v])/cpus for k, v in sums.items()})
-        result.update({k: child[k] for k in ['tag', 'file-path', 'function']})
+        result.update({k: child[k] for k in ['file-path', 'function']})
+
+        result['name'] = cls._normalise_tag_name(child['tag'])
         result['file-line'] = int(child['file-line'])
+        result['dur-ratio'] = float(child['cumul-time-min']) / float(child['cumul-time-max'])
         return result
