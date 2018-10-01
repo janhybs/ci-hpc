d8077d273f38f0d8d86196d84c52efe6e23a918a
Jan Hybs <jan.hybs@tul.cz>
2018-10-01 14:51:50 +0200

    Remove old views

--- ci-hpc/visualisation/main.py
+++ ci-hpc/visualisation/main.py
@@ -6,38 +6,12 @@ import os, sys
 from utils.logging import logger
 
 from visualisation.www import init_flask_server
-from visualisation.www.rest.report_view import ReportView
-from visualisation.www.rest.test_view import TestView
 from visualisation.www.rest.sparkline_view import SparklineView
-from visualisation.www.rest.scale_view import ScaleView
 from visualisation.www.rest.frame_view import FrameView
 from visualisation.www.rest.config_view import ConfigView
 
 
 api, app = init_flask_server()
-
-
-# api.add_resource(
-#     ReportView,
-#     '/<string:project>/report/<string:report_id>'
-# )
-#
-# api.add_resource(
-#     TestView,
-#     '/<string:project>/case-view/<string:options>',
-#     '/<string:project>/case-view',
-# )
-#
-# api.add_resource(
-#     ScaleView,
-#     '/<string:project>/scale-view/<string:base64data>',
-#     '/<string:project>/scale-view',
-# )
-#
-# api.add_resource(
-#     FrameView,
-#     '/<string:project>/case-view-detail/<string:ids>',
-# )
 
 
 api.add_resource(
