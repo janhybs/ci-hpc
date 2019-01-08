95cc1ba3de3cc5bf39e2c52466e0707e2f5a409b
Jan Hybs <jan.hybs@tul.cz>
2019-01-08 16:40:04 +0100

    Edit git field

--- cihpc/core/structures/project_step_git.py
+++ cihpc/core/structures/project_step_git.py
@@ -22,18 +22,30 @@ class ProjectStepGits(list):
         elif isinstance(kwargs, list):
             for v in kwargs:
                 if isinstance(v, str):
-                    self.append(ProjectStepGit(v))
+                    git_kw = self.default_kwargs(v)
+                    self.append(ProjectStepGit(**git_kw))
                 elif isinstance(v, dict):
-                    self.append(ProjectStepGit(**v))
+                    git_kw = self.default_kwargs(v['url'])
+                    git_kw.update(v)
+                    self.append(ProjectStepGit(**git_kw))
                 else:
                     raise ValueError('kwargs must be dictionary or list ro str')
 
         # just url
         elif isinstance(kwargs, str):
-            for v in kwargs:
-                self.append(ProjectStepGit(v))
+            git_kw = self.default_kwargs(kwargs)
+            self.append(ProjectStepGit(**git_kw))
         else:
             raise ValueError('kwargs must be dictionary or list ro str')
+
+    @staticmethod
+    def default_kwargs(url):
+        repo_name = str(os.path.basename(url).split('.')[0])
+        return dict(
+            url=url,
+            branch='<arg.branch.%s>' % repo_name,
+            commit='<arg.commit.%s>' % repo_name,
+        )
 
 
 class ProjectStepGit(object):
